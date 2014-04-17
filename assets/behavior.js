27253166d34ff36ddef7b01d416dcb5c0493e610
Ubuntu <navseeker7@gmail.com>
2014-04-17 06:24:43 +0000

    Generated documentation for d7f46205c9a4f64bd6845cbdb2bfc1ae1c6fdc0d

--- assets/behavior.js
+++ assets/behavior.js
@@ -92,7 +92,17 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "projectPath": "lib/log.coffee",
             "targetPath": "lib/log",
             "pageTitle": "lib/log",
-            "title": "log"
+            "firstHeader": {
+              "type": "heading",
+              "data": {
+                "level": 1,
+                "title": "Logger",
+                "slug": "logger",
+                "isFileHeader": true
+              },
+              "depth": 1
+            },
+            "title": "Logger"
           },
           "depth": 2,
           "outline": []
